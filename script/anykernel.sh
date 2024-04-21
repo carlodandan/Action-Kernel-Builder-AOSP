e14fad516df0d3de313299791dcf51b340e61582
Chris Renshaw <osm0sis@outlook.com>
2024-04-21 03:18:48 +0000

    yml: AnyKernel3: Added AnyKernel3 template support for flashable zip.
    Signed-off-by: Carlo Dandan <carlodandan.personal@proton.me>

---
+++ script/anykernel.sh
@@ -0,0 +1,36 @@
+### AnyKernel3 Ramdisk Mod Script
+## osm0sis @ xda-developers
+
+### AnyKernel setup
+# global properties
+properties() { '
+kernel.string=AnyKernel3 (generic) by osm0sis @ xda-developers
+do.devicecheck=0
+do.modules=0
+do.systemless=1
+do.cleanup=1
+do.cleanuponabort=0
+'; } # end properties
+
+
+### AnyKernel install
+## boot files attributes
+boot_attributes() {
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
+} # end attributes
+
+# boot shell variables
+block=auto;
+is_slot_device=auto;
+ramdisk_compression=auto;
+patch_vbmeta_flag=auto;
+
+# import functions/variables and setup patching - see for reference (DO NOT REMOVE)
+. tools/ak3-core.sh;
+
+# boot install
+dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
+## end boot install
